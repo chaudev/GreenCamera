c1f8bac023897b1adad8ea21f8f7a1290c4bc054
chaudev <soc900k@mail.com>
2021-08-09 13:22:36 +0700

    Mini Update

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View} from 'react-native';
 import {GreenCamera} from './src';
-import {checkMultiple, PERMISSIONS} from 'react-native-permissions';
+import {
+  checkMultiple,
+  PERMISSIONS,
+  requestMultiple,
+} from 'react-native-permissions';
 import {Colors} from 'green-native';
 
 const App = () => {
   const [permission, setPermission] = useState(false);
 
-  checkMultiple([
-    PERMISSIONS.ANDROID.CAMERA,
-    PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
-    PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
-  ]).then(statuses => {
-    if (statuses[PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE] == 'granted') {
-      setPermission(true);
-    }
-  });
+  useEffect(() => {
+    check();
+  }, []);
+
+  const check = () => {
+    requestMultiple([
+      PERMISSIONS.ANDROID.CAMERA,
+      PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+      PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE,
+    ]).then(statuses => {
+      if (statuses[PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE] == 'granted') {
+        setPermission(true);
+      } else {
+        check();
+      }
+    });
+  };
 
   return (
     <View style={{flex: 1, backgroundColor: Colors.black}}>
